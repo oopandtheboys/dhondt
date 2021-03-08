ab6930b875c28716a17bf1de2b2f4f0bf58914b9
Ross <rosselliman@hotmail.com>
2021-03-08 02:12:03 +0000

    Move D'Hondt calculation code from Program.cs into class.

--- Dhondt.cs
+++ Dhondt.cs
@@ -38,5 +38,37 @@ namespace CMP1903M
                     partyData.GetRange(2, partyData.Count - 2)));
             }
         }
+
+        private static Dictionary<string, int> _DictSort(Dictionary<string, int> toSort)
+        {
+            // Sort the dictionary to be returned
+            var ordered = toSort.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            return ordered;
+        }
+
+        public Dictionary<string, int> Calculate(Dictionary<string, int> parties, int rounds)
+        {
+            // Parties has an int component and a string component.
+            Dictionary<string, int> roundsWon = new Dictionary<string, int>();
+
+            // Create a dict for parties vs rounds won
+            for (int i = 0; i < parties.Count; i++)
+            {
+                roundsWon.Add(parties.ElementAt(i).Key, 0);
+            }
+
+            // Process the D'Hondt method
+            for (int i = 0; i < rounds; i++)
+            {
+                // Sort the dictionary
+                parties = _DictSort(parties);
+                // Increment the winner by one.
+                roundsWon[parties.ElementAt(parties.Count - 1).Key]++;
+                // Half the winner
+                parties[parties.ElementAt(parties.Count - 1).Key] /= 2;
+            }
+
+            return roundsWon;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,45 +11,13 @@ namespace CMP1903M
             // Proof of Concept
             var exampleObject = new Dhondt();
             exampleObject.ImportDataSet("Assessment1Data.txt");
-            Console.WriteLine($"{ exampleObject.TotalVotes } { exampleObject.Constituency } { exampleObject.Rounds }");
+            Console.WriteLine($"{exampleObject.TotalVotes} {exampleObject.Constituency} {exampleObject.Rounds}");
 
             foreach (var party in exampleObject.Parties)
             {
                 Console.WriteLine(party.Name);
                 Console.WriteLine(party.Votes);
             }
-        }
-
-        private static Dictionary<string, int> CalculateMethod(Dictionary<string, int> parties, int rounds)
-        {
-            // Parties has an int component and a string component.
-            Dictionary<string, int> roundsWon = new Dictionary<string, int>();
-
-            // Create a dict for parties vs rounds won
-            for (int i = 0; i < parties.Count; i++)
-            {
-                roundsWon.Add(parties.ElementAt(i).Key, 0);
-            }
-
-            // Process the D'Hondt method
-            for (int i = 0; i < rounds; i++)
-            {
-                // Sort the dictionary
-                parties = DictSort(parties);
-                // Increment the winner by one.
-                roundsWon[parties.ElementAt(parties.Count-1).Key]++;
-                // Half the winner
-                parties[parties.ElementAt(parties.Count - 1).Key] /= 2;
-            }
-
-            return roundsWon;
-        }
-
-        private static Dictionary<string, int> DictSort(Dictionary<string, int> toSort)
-        {
-            // Sort the dictionary to be returned
-            var ordered = toSort.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-            return ordered;
         }
     }
 }
